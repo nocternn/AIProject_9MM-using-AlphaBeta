0ccb5198c915e45322c366c0319bde4839ebbd6f
Duy Pham <duy.pt194749@sis.hust.edu.vn>
2021-12-25 00:39:38 +0700

    Bone of phases, Add ID, Test drag drop
    Add bone of game phases part
    Now can drag and drop piece "white1" around
    Add id for pieces and play positions
    
    PROBLEM: when drag over a play position (Ex: id "pos1_1"), piece is unmovable, because play pos is above circle
    
    IDEA TO DO: set play positions below play pieces, so we can always drag pieces

--- AI_Project/src/Main.java
+++ AI_Project/src/Main.java
@@ -2,13 +2,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 	private static Scene scene = null;
 
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
 		Pane root = FXMLLoader.load(getClass().getResource("Board.fxml"));
 		scene = new Scene(root, 800, 800);
 		
@@ -16,10 +16,37 @@ public class Main extends Application{
 		primaryStage.setTitle("Nine Men's Morris - by J7Defender");
 		primaryStage.setResizable(false);
 		primaryStage.show();
+		
+		//Test Drag and Drop, WORKED but with that 1 object only
+		Circle circle = (Circle) scene.lookup("#white1");
+		circle.setOnMouseClicked(event -> {
+			System.out.println("Drag Done");
+			
+		});
+		circle.setOnMouseDragged(event -> {
+			circle.setCenterX(event.getX());
+			circle.setCenterY(event.getY());
+		});
+	}
+	
+	private void OpeningPhase() {
+		
+	}
+	
+	private void MidGamePhase() {
+		
+	}
+	
+	private void EndGamePhase() {
+		
+	}
+	
+	private void GameEnd() {
+		
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		launch(args);
 	}
+	
 }
