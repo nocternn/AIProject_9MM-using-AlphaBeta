a68ded9f5e5eb992e85d2f1411e7cd4df39df1d6
Noctern <alexsky.gt12@gmail.com>
2021-12-25 13:36:37 +0700

    Data-binded game pieces

--- AI_Project/src/Main.java
+++ AI_Project/src/Main.java
@@ -1,12 +1,14 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 	private static Scene scene = null;
+	private Board board;
 
 	public void start(Stage primaryStage) throws Exception {
 		Pane root = FXMLLoader.load(getClass().getResource("Board.fxml"));
@@ -17,36 +19,51 @@ public class Main extends Application{
 		primaryStage.setResizable(false);
 		primaryStage.show();
 		
-		//Test Drag and Drop, WORKED but with that 1 object only
-		Circle circle = (Circle) scene.lookup("#white1");
-		circle.setOnMouseClicked(event -> {
-			System.out.println("Drag Done");
-			
-		});
-		circle.setOnMouseDragged(event -> {
-			circle.setCenterX(event.getX());
-			circle.setCenterY(event.getY());
-		});
-	}
-	
-	private void OpeningPhase() {
+		Group whitePieces = (Group) scene.lookup("#whitePieces");
+		Group blackPieces = (Group) scene.lookup("#blackPieces");
 		
-	}
-	
-	private void MidGamePhase() {
-		
-	}
-	
-	private void EndGamePhase() {
-		
-	}
-	
-	private void GameEnd() {
-		
+		// Data bind game pieces
+		board = new Board();
+		whitePieces.getChildren().setAll(FXCollections.observableList(board.whitePieces));
+		blackPieces.getChildren().setAll(FXCollections.observableList(board.blackPieces));
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
+		
+		// One thread for each game phase
+		Thread openingThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Game.OpeningPhase();
+			}});
+		Thread middleThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Game.MiddlePhase();
+			}});
+		Thread endingThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Game.EndingPhase();
+			}});
+		
+		try {
+			// First, start game opening phase
+			openingThread.start();
+			openingThread.join();
+			
+			// When opening phase is finished, start middle phase
+			middleThread.start();
+			middleThread.join();
+			
+			// When middle phase is finished, start ending phase
+			endingThread.start();
+			endingThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
