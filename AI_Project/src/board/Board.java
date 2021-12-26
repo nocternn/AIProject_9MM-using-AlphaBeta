124fa26d045f9d2703c7e33cbea2cbc81cadd9f0
Noctern <alexsky.gt12@gmail.com>
2021-12-26 21:06:46 +0700

    Added game phases. Updated board and game piece events.

--- AI_Project/src/board/Board.java
+++ AI_Project/src/board/Board.java
@@ -3,77 +3,76 @@ package board;
 
 import java.util.ArrayList;
 
+import game.Game;
+import helper.MoveListener;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.StrokeType;
-import javafx.scene.input.MouseEvent;
 
-
-public class Board {
-	public ArrayList<Circle> whitePieces = new ArrayList<Circle>();
-	public ArrayList<Circle> blackPieces = new ArrayList<Circle>();
+public class Board implements MoveListener {
+	public ArrayList<Piece> whitePieces = new ArrayList<Piece>();
+	public ArrayList<Piece> blackPieces = new ArrayList<Piece>();
+	
+	private static Piece[] board = new Piece[24];
 	
 	public Board() {
-		// Initialize black pieces,
+		// Initialize black pieces.
+		int blackIndex = 0;
 		for (double centerX = 821.0; centerX <= 821.0+14.0*9; centerX += 14.0) {
-			Circle circle = new Circle();
-
-			circle.setFill(Color.BLACK);
-			circle.setCenterX(centerX);
-            circle.setCenterY(118);
-			circle.setRadius(14.0);
-			circle.setStroke(Color.WHITESMOKE);
-			circle.setStrokeType(StrokeType.INSIDE);
-			circle.setStrokeWidth(0.3);
-			
-			this.blackPieces.add(circle);
+			this.blackPieces.add(new Piece(Color.BLACK, centerX, 118, Color.WHITESMOKE, blackIndex, this));
+			blackIndex++;
 		}
 		
 		// Initialize white pieces. Set drag and drop feature for white pieces.
+		int whiteIndex = 0;
 		for (double centerX = 218.0; centerX <= 218.0+14.0*9; centerX += 14.0) {
-			Circle circle = new Circle();
-			
-			circle.setFill(Color.WHITE);
-			circle.setCenterX(centerX);
-            circle.setCenterY(118);
-			circle.setRadius(14.0);
-			circle.setStroke(Color.BLACK);
-			circle.setStrokeType(StrokeType.INSIDE);
-			circle.setStrokeWidth(0.3);
-			
-			circle.setOnMousePressed(evt -> handleMousePress(evt, circle));
-            circle.setOnMouseDragged(evt -> handleMouseDrag(evt, circle));
-            circle.setOnMouseReleased(evt -> handleMouseRelease(evt, circle));
-			
-			this.whitePieces.add(circle);
+			this.whitePieces.add(new Piece(Color.WHITE, centerX, 118, Color.BLACK, whiteIndex, this));
+			whiteIndex++;
 		}
-
-		
 	}
 
-	//Drag and drop functions for white pieces
-	public void handleMousePress(MouseEvent evt, Circle circle){
-        circle.setFill(Color.YELLOW);
-    }
-    public void handleMouseDrag(MouseEvent evt, Circle circle){
-        circle.setCenterX(evt.getX());
-        circle.setCenterY(evt.getY());
-        for(int i=0;i<BoardController.boardPosition.size();i++) BoardController.boardPosition.get(i).setFill(Color.rgb(84, 255, 135));
-    }
-    public void handleMouseRelease(MouseEvent evt, Circle circle){
-		circle.setFill(Color.WHITE);
-        double releaseX = evt.getSceneX();
-        double releaseY = evt.getSceneY();
-        System.out.println("Drop location: " + releaseX + " " + releaseY);
-        for(int i=0;i<BoardController.boardPosition.size();i++){
-            double tempX = BoardController.boardPosition.get(i).getCenterX();
-            double tempY = BoardController.boardPosition.get(i).getCenterY();
-            if(releaseX >= tempX-50 && releaseX <= tempX+50 && releaseY >= tempY-50 && releaseY <= tempY+50){
-                circle.setCenterX(tempX);
-                circle.setCenterY(tempY);
-                System.out.println("Snapped to: " + i + " " + circle.getCenterX() + " " +  circle.getCenterY());
-                for(int j=0;j<BoardController.boardPosition.size();j++) BoardController.boardPosition.get(j).setFill(Color.TRANSPARENT);
-            }
-        }
-    }
+	@Override
+	public void movedWhitePiece(int pieceIndex, int initialPosition, int newPosition) {
+		// Delete piece at old position
+		if (initialPosition >= 0) {
+			board[initialPosition] = null;
+		}
+		// Add piece at new position
+		board[newPosition] = this.whitePieces.get(pieceIndex);
+		// Black's turn to make a move
+		switch (Game.getCurrentPhase()) {
+		case Opening:
+			putBlackPieceOnBoard();
+			break;
+		default:
+			slideBlackPieceOnBoard();
+		}
+		// Update game phase
+		Game.updateGamePhase();
+	}
+	
+	private void putBlackPieceOnBoard() {
+		System.out.println("Put black piece");
+	}
+
+	private void slideBlackPieceOnBoard() {
+		System.out.println("Slide black piece");
+	}
+
+	public static int getNumberWhiteOnBoard() {
+		int count = 0;
+		for (Piece piece : board) {
+			if (piece != null && piece.getColor() == Color.WHITE)
+				count++;
+		}
+		return count;
+	}
+
+	public static int getNumberBlackOnBoard() {
+		int count = 0;
+		for (Piece piece : board) {
+			if (piece != null && piece.getColor() == Color.BLACK)
+				count++;
+		}
+		return count;
+	}
+
 }
---
+++ AI_Project/src/helper/MoveListener.java
@@ -0,0 +1,5 @@
+package helper;
+
+public interface MoveListener {
+	void movedWhitePiece(int pieceIndex, int initialPosition, int newPosition);
+}
