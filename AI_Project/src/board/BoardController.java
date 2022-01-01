e4bd7924b9b193c0f2d16c90b891bd1e94a8692d
Jirachiii <trangdy1112@gmail.com>
2022-01-01 22:49:10 +0700

    add turn notification as group 'whiteTurn' and 'blackTurn' in Board.fxml

--- AI_Project/src/board/BoardController.java
+++ AI_Project/src/board/BoardController.java
@@ -2,6 +2,7 @@ package board;
 
 import javafx.fxml.*;
 import javafx.scene.layout.Pane;
+import javafx.scene.Group;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.*;
@@ -21,6 +22,8 @@ public class BoardController implements Initializable{
     private Circle pos1,pos2,pos3,pos4,pos5,pos6,pos7,pos8,pos9,pos10,pos11,pos12,pos13,pos14,pos15,pos16,pos17,pos18,pos19,pos20,pos21,pos22,pos23,pos24;
     @FXML
     private Pane pane;
+    @FXML
+    public Group whiteTurn, blackTurn;
     
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
@@ -34,5 +37,6 @@ public class BoardController implements Initializable{
 
         gameResult = new Text();
         pane.getChildren().add(gameResult);
+
     }    
 }
